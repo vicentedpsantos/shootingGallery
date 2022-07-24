7c21760066fda206c9d13c628ff0f0bead122d9a
Vicente Santos <vicentedpsantos@gmail.com>
2022-07-24 14:56:50 +0100

    Increase score if on target

--- main.lua
+++ main.lua
@@ -48,6 +48,15 @@ end
 
 function love.mousepressed(x, y, button, istouch, presses)
   if button == 1 then -- 1 is the primary mouse button
-    score = score + 1
+    local mouseToTarget = distanceBetween( { target.x, target.y }, { x, y } )
+
+    if mouseToTarget < target.radius then
+      score = score + 1
+    end
   end
+end
+
+-- distanceBetween({ x1, y1 }, { x2, y2 })
+function distanceBetween(coord1, coord2)
+  return math.sqrt( (coord2[1] - coord1[1])^2 + (coord2[2] - coord1[2])^2 )
 end
