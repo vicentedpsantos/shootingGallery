6045c76abd87a5f44976ca6f7023858e26ef762d
Vicente Santos <vicentedpsantos@gmail.com>
2022-07-24 14:15:16 +0100

    Draw target

--- main.lua
+++ main.lua
@@ -1,6 +1,5 @@
 -- Major variables are declared in this function
 function love.load()
-  number = 0
 end
 
 -- dt = Delta Time
@@ -8,10 +7,25 @@ end
 -- game. So in a 60 FPS game, this function would be called
 -- 60 times
 function love.update(dt)
-  number = number + 1
 end
 
 -- This function is responsible for drawing things on the screen
 function love.draw()
-  love.graphics.print(number)
+  drawTarget(200, 400)
+end
+
+-- drawCircle( x, y, radius, { 1, 0, 0 } )
+function drawCircle( x, y, radius, color )
+  love.graphics.setColor(color)
+  love.graphics.circle( "fill", x, y, radius, 100 )
+end
+
+local red = { 1, 0, 0 }
+local white = { 1, 1, 1 }
+function drawTarget(x, y)
+  drawCircle( x, y, 50, red )
+  drawCircle( x, y, 40, white )
+  drawCircle( x, y, 30, red )
+  drawCircle( x, y, 20, white )
+  drawCircle( x, y, 10, red )
 end
